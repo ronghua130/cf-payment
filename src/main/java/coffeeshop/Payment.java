5c615566826bf70afc753ce9b0da938976db29cd
SKCC <SKCC@SKCC18D00082.SKCC.NET>
2020-06-19 14:23:42 +0900

    ..

--- src/main/java/coffeeshop/Payment.java
+++ src/main/java/coffeeshop/Payment.java
@@ -33,11 +33,11 @@ public class Payment {
         // 1. order에서 Hystrix 를 설정 함:
         //    요청처리 쓰레드에서 처리시간이 610 밀리가 넘어서기 시작하여 어느정도 유지되면 CB 회로가 닫히도록 (요청을 빠르게 실패처리, 차단) 설정
         // 2. payment를 저장한 후 적당한 시간 끌기
-//        try {
-//            Thread.currentThread().sleep((long) (400 + Math.random() * 220));
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Thread.currentThread().sleep((long) (600 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
